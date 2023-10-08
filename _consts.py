52e8e9641ed00a632057b8f1b45b7d049f54f1e9
readyyyk <mr.sairexer@gmail.com>
2023-10-08 12:12:29 +0300

    [HF] - `Added`: changed application name to `chapy-server`

--- _consts.py
+++ _consts.py
@@ -5,6 +5,6 @@ NAME_PATTERN = compile(r"^\w{3,15}$")
 
 ALLOWED_CONNECTION_DETAILS = ["connected", "disconnected"]
 
-PROD_URL = ["r-chapy-server", "chapy-server-beta"]
+PROD_URL = ["chapy-server", "chapy-server-beta"]
 
 JWT_LIVE_TIME = 3
